127c8f477e526c4233602b4775ed4549784bed83
Virendra Prajapati <vp581082@gmail.com>
2024-12-26 15:53:27 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,23 @@
+public class Prime {
+
+    public static boolean isPrime(int num) {
+        if (num < 1) {
+            return false;
+        }
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for (int num = 1; num <= 100; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " , ");
+            }
+        }
+
+    }
+}
